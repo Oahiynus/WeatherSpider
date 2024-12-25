e191a7bc35c3cbd99848d5a213ab36a07a1b5d7a
Oahiynus <yh-sun@foxmail.com>
2024-12-25 21:38:58 +0800

    修改everyCity.py

--- WeatherSpider/spiders/everyCity.py
+++ WeatherSpider/spiders/everyCity.py
@@ -18,27 +18,23 @@ class EverycitySpider(scrapy.Spider):
     for url in findall(pattern, contents):
         start_urls.append(url[0])
 
-def parse(self, response):
-    # 处理每个城市的天气预报页面数据
-    item = WeatherspiderItem()
+    def parse(self, response):
+        # 处理每个城市的天气预报页面数据
+        item = WeatherspiderItem()
+        city = response.xpath('//div[@class="crumbs fl"]//a[2]//text()').extract()[0] 
+        item['city'] = city
+        # 每个页面只有一个城市的天气数据，直接取[0]
+        selector = response.xpath('//ul[@class="t clearfix"]')[0]
+        # 存放天气数据
+        weather = ''
+        for li in selector.xpath('./li'):
 
-    city = response.xpath('//div[@class="crumbs fl"]//a[2]//text()').extract()[0] 
-    item['city'] = city
-
-    # 每个页面只有一个城市的天气数据，直接取[0]
-    selector = response.xpath('//ul[@class="t clearfix"]')[0]
-
-    # 存放天气数据
-    weather = ''
-
-    for li in selector.xpath('./li'):
-
-        date = li.xpath('./h1//text()').extract()[0]
-        cloud = li.xpath('./p[@title]//text()').extract()[0]
-        high = li.xpath('./p[@class="tem"]//span//text()').extract()[0] 
-        low = li.xpath('./p[@class="tem"]//i//text()').extract()[0]
-        wind = li.xpath('./p[@class="win"]//em//span[1]/@title').extract()[0] 
-        wind = wind + li.xpath('./p[@class="win"]//i//text()').extract()[0]
-        weather = weather + date+':'+cloud+','+high+r'/'+low+','+wind+'\n'
-    item['weather'] = weather
-    return [item]
+            date = li.xpath('./h1//text()').extract()[0]
+            cloud = li.xpath('./p[@title]//text()').extract()[0]
+            high = li.xpath('./p[@class="tem"]//span//text()').extract()[0] 
+            low = li.xpath('./p[@class="tem"]//i//text()').extract()[0]
+            wind = li.xpath('./p[@class="win"]//em//span[1]/@title').extract()[0] 
+            wind = wind + li.xpath('./p[@class="win"]//i//text()').extract()[0]
+            weather = weather + date+':'+cloud+','+high+r'/'+low+','+wind+'\n'
+        item['weather'] = weather
+        return [item]

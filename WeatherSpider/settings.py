320daece073bdee77ab7f3cc441ebb50d69b0c84
Oahiynus <yh-sun@foxmail.com>
2024-12-26 00:02:53 +0800

    更新

--- WeatherSpider/settings.py
+++
@@ -1,6 +0,0 @@
-BOT_NAME = 'WeatherSpider'
-SPIDER_MODULES = ['WeatherSpider.spiders'] 
-NEWSPIDER_MODULE = 'WeatherSpider.spiders'
-ITEM_PIPELINES = {
-'WeatherSpider.pipelines.WeatherspiderPipeline':1,
-}
